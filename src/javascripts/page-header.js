12f922325034779ee35c3756acf766175fcf2b3d
ivan.khoteev <ivan.khoteev@saltpepper.ru>
2020-01-17 21:03:33 +0300

    Change header markup and css

--- src/javascripts/page-header.js
+++ src/javascripts/page-header.js
@@ -2,6 +2,7 @@ var pageHeader = document.querySelector('.page-header');
 var toggleButton = pageHeader.querySelector('.page-header__menu-toggle');
 var pageHeaderMenu = pageHeader.querySelector('.page-header__nav');
 
-toggleButton.onclick = function() {
+toggleButton.addEventListener('click', function() {
+  console.log(pageHeaderMenu);
   pageHeaderMenu.classList.toggle('main-nav--active');
-};
+});
