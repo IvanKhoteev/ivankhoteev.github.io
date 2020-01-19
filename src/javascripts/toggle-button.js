dba424e187f3177fb83116eac1bdaea39bc4a715
ivan.khoteev <ivan.khoteev@saltpepper.ru>
2020-01-19 11:03:47 +0300

    Finish mobile menu with animation

--- src/javascripts/toggle-button.js
+++ src/javascripts/toggle-button.js
@@ -4,8 +4,16 @@ const buttonLine1 = toggleButton.querySelector(".toggle-button__line1");
 const buttonLine2 = toggleButton.querySelector(".toggle-button__line2");
 const buttonLine3 = toggleButton.querySelector(".toggle-button__line3");
 
+const menu = document.querySelector('.page-header__nav');
+const menuItemWrappers = menu.querySelectorAll('.main-nav__item-wrapper');
+
 toggleButton.addEventListener('click', function() {
   buttonLine1.classList.toggle('toggle-button__line1--close');
   buttonLine2.classList.toggle('toggle-button__line2--close');
   buttonLine3.classList.toggle('toggle-button__line3--close');
+
+  for (var i = 0; i < menuItemWrappers.length; ++i) {
+    menuItemWrappers[i].classList.toggle('main-nav__item-wrapper-animated');
+  }
 });
+
