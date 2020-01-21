7286ea31020ee7232db11a4691a20528f51a19bb
ivan.khoteev <ivan.khoteev@saltpepper.ru>
2020-01-21 20:30:20 +0300

    Refactor code

--- src/javascripts/page-header.js
+++
@@ -1,7 +0,0 @@
-const pageHeader = document.querySelector('.page-header');
-const toggleButton = pageHeader.querySelector('.page-header__menu-toggle');
-const pageHeaderMenu = pageHeader.querySelector('.page-header__nav');
-
-toggleButton.addEventListener('click', function() {
-  pageHeaderMenu.classList.toggle('main-nav--active');
-});
