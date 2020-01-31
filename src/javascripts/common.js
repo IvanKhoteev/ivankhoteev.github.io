7107a30e4e5b3df9a222ce038d64ed8b7da03aa4
Ivan Khoteev <ivan.khoteev@saltpepper.ru>
2020-01-31 08:34:59 +0300

    Add animation to header block

--- src/javascripts/common.js
+++ src/javascripts/common.js
@@ -5,3 +5,16 @@ const promoIcons = document.querySelectorAll('.promo-icon__item');
 for (var i = 0; i < promoIcons.length; ++i) {
   promoIcons[i].classList.add('promo-icon__item--animated');
 }
+
+let scroll = window.scrollY;
+const header = document.querySelector('.page-header');
+
+window.addEventListener('scroll', function(e) {
+  if (window.scrollY - scroll > 50) {
+    header.classList.add('page-header--hidden');
+    scroll = window.scrollY;
+  } else if (scroll - window.scrollY > 10) {
+    header.classList.remove('page-header--hidden');
+    scroll = window.scrollY;
+  }
+})
