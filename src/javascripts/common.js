c5943e67d8aab07f2c3b923467cbfbaa238c6830
Ivan Khoteev <ivan.khoteev@saltpepper.ru>
2020-02-19 08:45:32 +0300

    Add base animations to desktop version

--- src/javascripts/common.js
+++ src/javascripts/common.js
@@ -1,5 +1,12 @@
 const promo = document.querySelector('.page-main__promo');
+const galleryMain = document.querySelector('.page-main__gallery-main-wrapper');
+const aboutMain = document.querySelector('.about-main');
+const aboutAdditional = document.querySelector('.page-main__about-additional');
+const gotoMain = document.querySelector('.goto-main');
+const gotoAdditional = document.querySelector('.page-main__goto-additional');
 promo.classList.add('page-main__promo--animated');
+galleryMain.classList.add('page-main__gallery-main-wrapper--animated');
+
 
 const promoIcons = document.querySelectorAll('.promo-icon__item');
 for (var i = 0; i < promoIcons.length; ++i) {
@@ -16,5 +23,20 @@ window.addEventListener('scroll', function(e) {
   } else if (scroll - window.scrollY > 10) {
     header.classList.remove('page-header--hidden');
     scroll = window.scrollY;
+  }
+
+  const aboutWrapper = document.querySelector('.page-main__about-main-wrapper');
+  const aboutWrapperPosition = aboutWrapper.getBoundingClientRect();
+  const gotoWrapper = document.querySelector('.page-main__goto-main-wrapper');
+  const gotoWrapperPosition = gotoWrapper.getBoundingClientRect();
+
+  if (aboutWrapperPosition.top < window.innerHeight && aboutWrapperPosition.bottom >= 0) {
+    aboutMain.classList.add('about-main--animated');
+    aboutAdditional.classList.add('page-main__about-additional--animated');
+  }
+
+  if (gotoWrapperPosition.top < window.innerHeight && gotoWrapperPosition.bottom >= 0) {
+    gotoMain.classList.add('goto-main--animated');
+    gotoAdditional.classList.add('page-main__goto-additional--animated');
   }
 })
