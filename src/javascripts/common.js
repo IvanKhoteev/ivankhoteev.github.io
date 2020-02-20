bc781c363c901213b0604c310c68658739b8f84a
Ivan Khoteev <ivan.khoteev@saltpepper.ru>
2020-02-20 10:18:16 +0300

    Fix hide menu on scroll

--- src/javascripts/common.js
+++ src/javascripts/common.js
@@ -31,31 +31,3 @@ const promoIcons = document.querySelectorAll('.promo-icon__item');
 for (var i = 0; i < promoIcons.length; ++i) {
   promoIcons[i].classList.add('promo-icon__item--animated');
 }
-
-let scroll = window.scrollY;
-const header = document.querySelector('.page-header');
-
-window.addEventListener('scroll', function(e) {
-  if (window.scrollY - scroll > 50) {
-    header.classList.add('page-header--hidden');
-    scroll = window.scrollY;
-  } else if (scroll - window.scrollY > 10) {
-    header.classList.remove('page-header--hidden');
-    scroll = window.scrollY;
-  }
-
-  const aboutWrapper = document.querySelector('.page-main__about-main-wrapper');
-  const aboutWrapperPosition = aboutWrapper.getBoundingClientRect();
-  const gotoWrapper = document.querySelector('.page-main__goto-main-wrapper');
-  const gotoWrapperPosition = gotoWrapper.getBoundingClientRect();
-
-  if (aboutWrapperPosition.top < window.innerHeight && aboutWrapperPosition.bottom >= 0) {
-    aboutMain.classList.add('about-main--animated');
-    aboutAdditional.classList.add('page-main__about-additional--animated');
-  }
-
-  if (gotoWrapperPosition.top < window.innerHeight && gotoWrapperPosition.bottom >= 0) {
-    gotoMain.classList.add('goto-main--animated');
-    gotoAdditional.classList.add('page-main__goto-additional--animated');
-  }
-})
